6326c747d438492466c317ab22173a89aaa8916a
Mohammed Ahnaf Abid <mohammed.abid@northsouth.edu>
2020-11-12 15:28:07 +0600

    Added classtask and screenshot for Lab02

--- Lab02/src/lab02/Lab02.java
+++ Lab02/src/lab02/Lab02.java
@@ -14,10 +14,51 @@ public class Lab02 {
     /**
      * @param args the command line arguments
      */
-    //TASK 1
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("---------CLASSWORK----------");
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter any integer:");
+        int num=input.nextInt();
+        if(num==0)
+            System.out.println("You have entered zero");
+        else
+            System.out.println("You have entered non-zero");
+        
+        System.out.println("Enter any integers: ");
+        int num1=input.nextInt();
+        System.out.println("Please enter " +num1);
+        int num2=input.nextInt();
+        System.out.println("Please enter " +num2);
+        
+        if(num1>num2)
+            System.out.println(num1 + " is greater than " + num2);
+        else if (num2>num1)
+            System.out.println(num2 +" is greater than" +num1);
+        else
+            System.out.println(num1 + " and " + num2 + " are equal ");
+        
+        System.out.println("Enter any integer: ");
+        int num3 = input.nextInt();
+        
+        if(num3>=0)
+            System.out.println("Input is non-negative ");
+        else
+            System.out.println("Input is negative ");
+        
+        System.out.println("Enter any integer: ");
+        float num4=input.nextFloat();
+        if(num4>=0)
+            System.out.println("Input is non-negative ");
+        if(num4>=1)
+            System.out.println("Input is also a natural number");
+        else
+            System.out.println("Input is negative ");
+        
+        
+        //TASK 1
+        // TODO code application logic here
+        
         System.out.println("Please enter an integer to check for even or odd: ");
         int userGivenInteger = input.nextInt();
         int result= userGivenInteger%2;
@@ -53,10 +94,12 @@ public class Lab02 {
                 
         }
         System.out.println("Please enter decimal number for BMI check: ");
-        float userGivenInteger3 = input.nextFloat();
+        
      
         
         //TASK 3 WITH if-else if-else
+        System.out.println("IF ELSE");
+        float userGivenInteger3 = input.nextFloat();
         if(userGivenInteger3<18.5){
             System.out.println("underweight");
         }
@@ -83,6 +126,7 @@ public class Lab02 {
         }
             
         }
+        System.out.println("TERNARY OPERATOR");
         //TASK 3 WITH TERNARY OPERATOR
         String compare1= userGivenInteger3<18.5? "underweight":"";
         System.out.println(compare1);
